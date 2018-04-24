5a09feea5ef0159153e38fdb404e5f056217bf16
asya-schtirlitz <i+github@homb.it>
2018-04-24 17:08:35 +0300

    nested dictionaries task
    letter "Питон-3" April 13

---
+++ python_HW/stores.py
@@ -0,0 +1,23 @@
+
+
+def create_dict(file_name):
+    stores = {}
+    with open(file_name, encoding='utf-8') as fd:
+        for line in fd:
+            store_name, shelf_number, product = line.split()
+            shelves = stores.setdefault(store_name, {})
+            shelf = shelves.setdefault(shelf_number,set())
+            shelf.add(product)
+    return stores
+
+def find_product(stores, product_name):
+	for store, shelves in stores.items():
+		for shelf, products in shelves.items():
+			if product_name in products:
+				return "Store: {0}, shelf: {1}".format(store, shelf)
+	return None
+
+if __name__ == '__main__':
+	stores = create_dict("table.csv")
+	print(find_product(stores, "ПЕРФОФАЙЛЫ"))
+	print(find_product(stores, "ПАПКА"))
